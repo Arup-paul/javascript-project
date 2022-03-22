bd1619f7a735410499cdb1dbb0b9bcba1e38810f
arup-paul <aruppaulctg@gmail.com>
2022-03-22 16:07:42 +0600

    cart content append

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -72,18 +72,50 @@ class UI {
                     event.target.disabled = true;
 
                     //get product from products
-                    let cartItem = {...Storage.getProduct(id),anount:1};
-
-
+                    let cartItem = {...Storage.getProduct(id),amount:1};
                     //add product to cart
+                    cart = [...cart,cartItem];
                     //save cart in local storage
+                    Storage.saveCart(cart)
                     //set cart values
+                    this.setCartValues(cart)
                     //display cart item
+                    this.addCartItem(cartItem)
                     //show the cart
                 })
             }
         })
 
+    }
+    setCartValues(cart){
+     let tempTotal = 0;
+     let itemsTotal = 0;
+     cart.map(item => {
+         tempTotal += item.price * item.amount;
+         itemsTotal += item.amount
+     })
+        cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+        cartItems.innerText = itemsTotal;
+    }
+    addCartItem(item){
+      const div = document.createElement('div');
+      div.classList.add('cart-item');
+      div.innerHTML = `
+      <img src=${item.image} alt="product">
+                  <div>
+                      <h4>${item.title}</h4>
+                      <h5>$${item.price}</h5>
+                      <span class="remove-item" data-id=${item.id}>remove</span>
+                  </div>
+              </div>
+              <div>
+                  <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                  <p class="item-amount">${item.amount}</p>
+                  <i class="fas fa-chevron-down" data-id=${item.id}></i>
+              </div>
+      `;
+        cartContent.appendChild(div)
+        console.log(cartContent)
     }
 }
 
@@ -97,6 +129,9 @@ class Storage{
      let products = JSON.parse(localStorage.getItem('products'));
      return products.find(product => product.id === id)
  }
+  static saveCart(cart){
+     localStorage.setItem('cart',JSON.stringify((cart)))
+    }
 }
 
 document.addEventListener("DOMContentLoaded",() => {
